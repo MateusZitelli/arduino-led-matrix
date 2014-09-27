0b029811a05c026e4386ae550e31c74fbf628df0
Ciro S. Costa <ciro.costa@usp.br>
2014-09-27 03:20:42 -0300

    updates toBin

--- led_matrix_ino/to-bin.c
+++ led_matrix_ino/to-bin.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 #define ROW_SIZE 10
+
+bool equalStrings(char * a, char * b)
+{
+	return (strcmp(a, b) != 0) ? false : true;
+}
 
 /**
  * Converts an integer value to a string given a
@@ -16,9 +23,7 @@ char * toBin(int value, int bitsCount, char * str)
 
     str[bitsCount] = '\0';
     for (i = bitsCount - 1; i >= 0; --i, value >>= 1)
-    {
-        str[i] = (value & 1) + '0';
-    }
+      str[i] = (value & 1) + '0';
 
     return str;
 }
@@ -28,8 +33,15 @@ int main(int argc, char const *argv[])
 	int value = 34;
 	int size = ROW_SIZE;
 	char repr[ROW_SIZE];
+	char * expected = "0000100010";
 
-	printf("%s\n", toBin(value, size, repr));
+	toBin(value, size, repr);
+
+	if (!equalStrings(repr, expected)) {
+		printf("%s\n", "Error! Expected binary does not match actual binary");
+
+		return 1;
+	}
 
 	return 0;
 }
