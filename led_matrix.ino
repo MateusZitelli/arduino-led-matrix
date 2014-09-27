79fc5c9e3b4fdee6b238c21290008f796d5e436a
Mateus Zitelli <zitellimateus@gmail.com>
2014-09-27 00:31:01 -0300

    Change project to support 10x10 matrix

--- led_matrix.ino
+++ led_matrix.ino
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MATRIX_SIZE 2
+#define MATRIX_SIZE 10
 
 //Pin connected to latch pin (ST_CP) of 74HC595
 const int latchPin = 8;
@@ -8,10 +8,10 @@ const int clockPin = 13;
 ////Pin connected to Data in (DS) of 74HC595
 const int dataPin = 12;
 
-int matrix[MATRIX_SIZE];
+int matrix[MATRIX_SIZE][MATRIX_SIZE];
 
 void setup() {
-  int i;
+  int i,j;
   //set pins to output because they are addressed in the main loop
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);  
@@ -19,35 +19,43 @@ void setup() {
   Serial.begin(9600);
   Serial.println("reset");
   // Initialize matrix
-  for(i = 0; i < MATRIX_SIZE; i++){
-    matrix[i] = 0; 
+  for(j = 0; j < MATRIX_SIZE; j++){
+    for(i = 0; i < MATRIX_SIZE; i++){
+      matrix[j][i] = (i == j || i == MATRIX_SIZE - j - 1);
+    }
   }
 }
 
 void loop() {
-  checkSerial();
   show();
+  checkSerial();
 }
 
 void checkSerial(){
-  // Check the serial buffer for arrays instructions
-  int i, j, line;
-  // Check if there is enouth bytes in the buffer
-  if(Serial.available() >= MATRIX_SIZE * MATRIX_SIZE){
-    for(j = 0; j < MATRIX_SIZE; j++){
-      line = 0;
-      // Create an int with the first MATIX_SIZE bits representing each led in a
-      // line, with states between ON (input != 0) and off (input = 0)
-      for(i = 0; i < MATRIX_SIZE; i++){
-        line = line << 1;
-        // Subtract 48 from the byte to convert ASCII decimals representation
-        // to int.
-        line += (Serial.read() - 48) || 0;
+  // Check in the serial buffer for 100 bytes to describe the matrix
+  int i, j;
+  int column = 0;
+  int line = 0;
+  int readSerial = 1;
+  byte dataByte;
+  // 13 bytes to store 100 bits
+  if(Serial.available() >= 13){
+    while(readSerial){
+      dataByte = Serial.read();
+      Serial.write(dataByte);
+      for(i = 0; i < 8; i++){
+        matrix[line][column++] = dataByte & (1 << i);
+        if(column >= MATRIX_SIZE){
+          column = 0;
+          line++;
+          if(line >= MATRIX_SIZE){
+            readSerial = 0;
+            break; 
+          }
+        }
       }
-      // Add the loaded line to the matrix
-      matrix[j] = line;
     }
-  } 
+  }
 }
 
 void show(){
@@ -59,25 +67,28 @@ void show(){
   // cathodes, each line is activated setting it to an off state while the
   // other lines are on. With one line activated a specific led from it could be
   // turned on just aplying current on their cathod column.
-  int j;
+  int i, j;
   // An int with MATRIX_SIZE bits equal 1
   int anodesMask = 1 << (MATRIX_SIZE) - 1;
-  int data;
+  long data;
   for(j = 0; j < MATRIX_SIZE; j++){
-    // Iterate line by line
-    data = 0;
-    // Turn off the bit related w/ the line that will be turned on
-    data = anodesMask - (1 << (j - 1));
-    // Put the columns bits after the anodes ones
-    data += matrix[j] << MATRIX_SIZE;
-    // Turn on the line
     digitalWrite(latchPin, 0);
-    shiftOut(dataPin, clockPin, data);
+    shiftOut(dataPin, clockPin, j);
     digitalWrite(latchPin, 1);
   }
 }
 
-void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
+void shiftBit(int myDataPin, int myClockPin, byte b){
+  digitalWrite(myClockPin, 0);
+  //Sets the pin to HIGH or LOW depending on bit
+  digitalWrite(myDataPin, b); 
+  //register shifts bits on upstroke of clock pin  
+  digitalWrite(myClockPin, 1);
+  //zero the data pin after shift to prevent bleed through
+  digitalWrite(myDataPin, 0);
+}
+
+void shiftOut(int myDataPin, int myClockPin, int line) {
   // This shifts 8 bits out MSB first,
   //on the rising edge of the clock,
   //clock idles low
@@ -87,36 +98,20 @@ void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
   int pinState;
   pinMode(myClockPin, OUTPUT);
   pinMode(myDataPin, OUTPUT);
-
-  //clear everything out just in case to
-  //prepare shift register for bit shifting
-  digitalWrite(myDataPin, 0);
-  digitalWrite(myClockPin, 0);
-
-  //for each bit in the byte myDataOut�
-  //NOTICE THAT WE ARE COUNTING DOWN in our for loop
-  //This means that %00000001 or "1" will go through such
-  //that it will be pin Q0 that lights.
-  for (i=7; i>=0; i--)  {
-    digitalWrite(myClockPin, 0);
-
-    //if the value passed to myDataOut and a bitmask result
-    // true then... so if we are at i=6 and our value is
-    // %11010100 it would the code compares it to %01000000
-    // and proceeds to set pinState to 1.
-    if ( myDataOut & (1<<i) ) {
-      pinState= 1;
-    }
-    else { 
-      pinState= 0;
-    }
-
-    //Sets the pin to HIGH or LOW depending on pinState
-    digitalWrite(myDataPin, pinState);
-    //register shifts bits on upstroke of clock pin  
-    digitalWrite(myClockPin, 1);
-    //zero the data pin after shift to prevent bleed through
-    digitalWrite(myDataPin, 0);
+  
+  // Set the 10th line state - because of a mistake in the
+  // circuit board the first bit is always sent to define
+  // the 10th line.
+  shiftBit(myDataPin, myClockPin, line != 9);
+  
+  // Set the columns states
+  for (i= MATRIX_SIZE - 1; i >= 0; i--)  {
+    shiftBit(myDataPin, myClockPin, matrix[line][i]); 
+  }
+  
+  //Set the remaining lines
+  for(i= MATRIX_SIZE - 2; i >= 0; i--){
+    shiftBit(myDataPin, myClockPin, i != line);
   }
 
   //stop shifting
